f0fb17704c342fbca5f9919db1fcc6bd428e792d
acwhelan <acwhelan@usc.edu>
2024-02-24 19:15:38 -0800

    PVD shadows import error

--- SQDMetal/__init__.py
+++ SQDMetal/__init__.py
@@ -1,3 +1,3 @@
-from .PVD_Shadows import PVD_Shadows
+from SQDMetal.Utilities.PVD_Shadows import PVD_Shadows
 from SQDMetal.COMSOL.Model import COMSOL_Model
 from SQDMetal.COMSOL.SimCapacitance import COMSOL_Simulation_CapMats
