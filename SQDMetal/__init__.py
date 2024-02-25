4790c39c6601b5cc7d42603b3b5f9a412f7042b6
acwhelan <acwhelan@usc.edu>
2024-02-24 18:44:09 -0800

    fixing import structure again

--- SQDMetal/__init__.py
+++ SQDMetal/__init__.py
@@ -1,3 +1,3 @@
-from Utilities.PVD_Shadows import PVD_Shadows
+from .Utilities.PVD_Shadows import PVD_Shadows
 from SQDMetal.COMSOL.Model import COMSOL_Model
 from SQDMetal.COMSOL.SimCapacitance import COMSOL_Simulation_CapMats
